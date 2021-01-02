4c7bde129704a74dfc6130063c959b3bcb78d355
ZetPin <Usuari@149.34.0.55>
2021-01-03 00:34:29 +0100

    Première maj

---
+++ src/main/java/net/mcreator/alltheelements/block/NikelOreBlock.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.alltheelements.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.alltheelements.AllTheElementsModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@AllTheElementsModElements.ModElement.Tag
+public class NikelOreBlock extends AllTheElementsModElements.ModElement {
+	@ObjectHolder("all_the_elements:nikel_ore")
+	public static final Block block = null;
+	public NikelOreBlock(AllTheElementsModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1f, 10f).lightValue(0).harvestLevel(1)
+					.harvestTool(ToolType.PICKAXE));
+			setRegistryName("nikel_ore");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 0));
+		}
+	}
+}
