42e01fb1211d90e54c2837ce867c05c7c8feabc9
Zetpin <Thresharks@192.168.1.43>
2021-01-03 20:13:51 +0100

    Patch 2.4 : Fixed Zirconium

--- src/main/java/net/mcreator/alltheelements/item/Zirconium_ToolsHoeItem.java
+++ src/main/java/net/mcreator/alltheelements/item/Zirconium_ToolsHoeItem.java
@@ -1,12 +1,21 @@
 
 package net.mcreator.alltheelements.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.HoeItem;
+
+import net.mcreator.alltheelements.AllTheElementsModElements;
+
 @AllTheElementsModElements.ModElement.Tag
 public class Zirconium_ToolsHoeItem extends AllTheElementsModElements.ModElement {
-
 	@ObjectHolder("all_the_elements:zirconium_tools_hoe")
 	public static final Item block = null;
-
 	public Zirconium_ToolsHoeItem(AllTheElementsModElements instance) {
 		super(instance, 67);
 	}
@@ -38,8 +47,6 @@ public class Zirconium_ToolsHoeItem extends AllTheElementsModElements.ModElement
 				return Ingredient.fromStacks(new ItemStack(ZirconiumIngotItem.block, (int) (1)));
 			}
 		}, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
-
 		}.setRegistryName("zirconium_tools_hoe"));
 	}
-
 }
