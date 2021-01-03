39d7ce84fae7c8ed5b28062952fe47ec4cc412b4
ZetPin <Usuari@149.34.0.55>
2021-01-03 20:08:41 +0100

    Vingt-Quatrième MAJ : Ajout Du bloc de nickel

---
+++ src/main/java/net/mcreator/alltheelements/block/BlockOfNickelBlock.java
@@ -0,0 +1,55 @@
+
+package net.mcreator.alltheelements.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.PushReaction;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.alltheelements.AllTheElementsModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@AllTheElementsModElements.ModElement.Tag
+public class BlockOfNickelBlock extends AllTheElementsModElements.ModElement {
+	@ObjectHolder("all_the_elements:block_of_nickel")
+	public static final Block block = null;
+	public BlockOfNickelBlock(AllTheElementsModElements instance) {
+		super(instance, 60);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1f, 10f).lightValue(0));
+			setRegistryName("block_of_nickel");
+		}
+
+		@Override
+		public PushReaction getPushReaction(BlockState state) {
+			return PushReaction.BLOCK;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(BlockOfNickelBlock.block, (int) (1)));
+		}
+	}
+}
