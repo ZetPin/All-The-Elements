93f93c3535f41d7dfc81ab3b379cb4863109bf94
ZetPin <Usuari@149.34.0.55>
2021-01-03 20:28:58 +0100

    Trente-et-Unième MAJ :  AJOUT D'UN ONGLET POUR LE MOD

---
+++ src/main/java/net/mcreator/alltheelements/itemgroup/AllTheElementsItemGroup.java
@@ -0,0 +1,35 @@
+
+package net.mcreator.alltheelements.itemgroup;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+
+import net.mcreator.alltheelements.block.NikelOreBlock;
+import net.mcreator.alltheelements.AllTheElementsModElements;
+
+@AllTheElementsModElements.ModElement.Tag
+public class AllTheElementsItemGroup extends AllTheElementsModElements.ModElement {
+	public AllTheElementsItemGroup(AllTheElementsModElements instance) {
+		super(instance, 80);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new ItemGroup("taball_the_elements") {
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public ItemStack createIcon() {
+				return new ItemStack(NikelOreBlock.block, (int) (1));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasSearchBar() {
+				return true;
+			}
+		}.setBackgroundImageName("item_search.png");
+	}
+	public static ItemGroup tab;
+}
