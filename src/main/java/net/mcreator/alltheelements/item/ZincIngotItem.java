797f0ba7559475fb6ee72a318271580e7bd6a959
Zetpin <Thresharks@192.168.1.43>
2021-01-03 20:54:45 +0100

    Trente-deuxième MAJ : ajout du minerai et du lingot de Zinc

---
+++ src/main/java/net/mcreator/alltheelements/item/ZincIngotItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.alltheelements.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.alltheelements.AllTheElementsModElements;
+
+@AllTheElementsModElements.ModElement.Tag
+public class ZincIngotItem extends AllTheElementsModElements.ModElement {
+	@ObjectHolder("all_the_elements:zinc_ingot")
+	public static final Item block = null;
+	public ZincIngotItem(AllTheElementsModElements instance) {
+		super(instance, 82);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MATERIALS).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("zinc_ingot");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
