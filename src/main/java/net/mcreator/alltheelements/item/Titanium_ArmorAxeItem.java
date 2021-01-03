9844e7143b32614d506a10833048626996d8ad34
Zetpin <Thresharks@192.168.1.43>
2021-01-03 19:44:56 +0100

    Vingt-deuxième MAJ : Ajout du TItane (minerai/lingot/outils/armures)

---
+++ src/main/java/net/mcreator/alltheelements/item/Titanium_ArmorAxeItem.java
@@ -0,0 +1,52 @@
+
+package net.mcreator.alltheelements.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.AxeItem;
+
+import net.mcreator.alltheelements.AllTheElementsModElements;
+
+@AllTheElementsModElements.ModElement.Tag
+public class Titanium_ArmorAxeItem extends AllTheElementsModElements.ModElement {
+	@ObjectHolder("all_the_elements:titanium_armor_axe")
+	public static final Item block = null;
+	public Titanium_ArmorAxeItem(AllTheElementsModElements instance) {
+		super(instance, 50);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new AxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 250;
+			}
+
+			public float getEfficiency() {
+				return 6f;
+			}
+
+			public float getAttackDamage() {
+				return 0f;
+			}
+
+			public int getHarvestLevel() {
+				return 2;
+			}
+
+			public int getEnchantability() {
+				return 14;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(TitaniumIngotItem.block, (int) (1)));
+			}
+		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("titanium_armor_axe"));
+	}
+}
