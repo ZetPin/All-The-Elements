24e3d51e58158c7bbc52720c9ee7b1d095d176f5
ZetPin <Usuari@91.126.230.52>
2021-01-08 15:39:24 +0100

    Patch 4.3 :  FORGE ADVANCEMENTS

--- src/main/java/net/mcreator/alltheelements/gui/ExplainationGui.java
+++ src/main/java/net/mcreator/alltheelements/gui/ExplainationGui.java
@@ -3,10 +3,8 @@ package net.mcreator.alltheelements.gui;
 
 import org.lwjgl.opengl.GL11;
 
-import net.minecraftforge.items.SlotItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 import net.minecraftforge.items.IItemHandler;
-import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.fml.network.NetworkEvent;
 import net.minecraftforge.fml.network.IContainerFactory;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -20,23 +18,18 @@ import net.minecraft.world.World;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.network.PacketBuffer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.inventory.container.Slot;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.inventory.container.Container;
-import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.Entity;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.Minecraft;
 
-import net.mcreator.alltheelements.procedures.TestProcedure;
-import net.mcreator.alltheelements.procedures.OpenForgeHelpGuiProcedure;
+import net.mcreator.alltheelements.procedures.OpenForgeGuiProcedure;
 import net.mcreator.alltheelements.AllTheElementsModElements;
 import net.mcreator.alltheelements.AllTheElementsMod;
 
@@ -45,11 +38,11 @@ import java.util.Map;
 import java.util.HashMap;
 
 @AllTheElementsModElements.ModElement.Tag
-public class ForgeGuiGui extends AllTheElementsModElements.ModElement {
+public class ExplainationGui extends AllTheElementsModElements.ModElement {
 	public static HashMap guistate = new HashMap();
 	private static ContainerType<GuiContainerMod> containerType = null;
-	public ForgeGuiGui(AllTheElementsModElements instance) {
-		super(instance, 162);
+	public ExplainationGui(AllTheElementsModElements instance) {
+		super(instance, 164);
 		elements.addNetworkMessage(ButtonPressedMessage.class, ButtonPressedMessage::buffer, ButtonPressedMessage::new,
 				ButtonPressedMessage::handler);
 		elements.addNetworkMessage(GUISlotChangedMessage.class, GUISlotChangedMessage::buffer, GUISlotChangedMessage::new,
@@ -65,7 +58,7 @@ public class ForgeGuiGui extends AllTheElementsModElements.ModElement {
 
 	@SubscribeEvent
 	public void registerContainer(RegistryEvent.Register<ContainerType<?>> event) {
-		event.getRegistry().register(containerType.setRegistryName("forge_gui"));
+		event.getRegistry().register(containerType.setRegistryName("explaination"));
 	}
 	public static class GuiContainerModFactory implements IContainerFactory {
 		public GuiContainerMod create(int id, PlayerInventory inv, PacketBuffer extraData) {
@@ -84,7 +77,7 @@ public class ForgeGuiGui extends AllTheElementsModElements.ModElement {
 			super(containerType, id);
 			this.entity = inv.player;
 			this.world = inv.player.world;
-			this.internal = new ItemStackHandler(3);
+			this.internal = new ItemStackHandler(0);
 			BlockPos pos = null;
 			if (extraData != null) {
 				pos = extraData.readBlockPos();
@@ -92,53 +85,6 @@ public class ForgeGuiGui extends AllTheElementsModElements.ModElement {
 				this.y = pos.getY();
 				this.z = pos.getZ();
 			}
-			if (pos != null) {
-				if (extraData.readableBytes() == 1) { // bound to item
-					byte hand = extraData.readByte();
-					ItemStack itemstack;
-					if (hand == 0)
-						itemstack = this.entity.getHeldItemMainhand();
-					else
-						itemstack = this.entity.getHeldItemOffhand();
-					itemstack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(capability -> {
-						this.internal = capability;
-						this.bound = true;
-					});
-				} else if (extraData.readableBytes() > 1) {
-					extraData.readByte(); // drop padding
-					Entity entity = world.getEntityByID(extraData.readVarInt());
-					if (entity != null)
-						entity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(capability -> {
-							this.internal = capability;
-							this.bound = true;
-						});
-				} else { // might be bound to block
-					TileEntity ent = inv.player != null ? inv.player.world.getTileEntity(pos) : null;
-					if (ent != null) {
-						ent.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).ifPresent(capability -> {
-							this.internal = capability;
-							this.bound = true;
-						});
-					}
-				}
-			}
-			this.customSlots.put(0, this.addSlot(new SlotItemHandler(internal, 0, 18, 49) {
-			}));
-			this.customSlots.put(1, this.addSlot(new SlotItemHandler(internal, 1, 18, 14) {
-			}));
-			this.customSlots.put(2, this.addSlot(new SlotItemHandler(internal, 2, 45, 31) {
-				@Override
-				public boolean isItemValid(ItemStack stack) {
-					return false;
-				}
-			}));
-			int si;
-			int sj;
-			for (si = 0; si < 3; ++si)
-				for (sj = 0; sj < 9; ++sj)
-					this.addSlot(new Slot(inv, sj + (si + 1) * 9, 0 + 8 + sj * 18, 0 + 84 + si * 18));
-			for (si = 0; si < 9; ++si)
-				this.addSlot(new Slot(inv, si, 0 + 8 + si * 18, 0 + 142));
 		}
 
 		public Map<Integer, Slot> get() {
@@ -148,148 +94,6 @@ public class ForgeGuiGui extends AllTheElementsModElements.ModElement {
 		@Override
 		public boolean canInteractWith(PlayerEntity player) {
 			return true;
-		}
-
-		@Override
-		public ItemStack transferStackInSlot(PlayerEntity playerIn, int index) {
-			ItemStack itemstack = ItemStack.EMPTY;
-			Slot slot = (Slot) this.inventorySlots.get(index);
-			if (slot != null && slot.getHasStack()) {
-				ItemStack itemstack1 = slot.getStack();
-				itemstack = itemstack1.copy();
-				if (index < 3) {
-					if (!this.mergeItemStack(itemstack1, 3, this.inventorySlots.size(), true)) {
-						return ItemStack.EMPTY;
-					}
-					slot.onSlotChange(itemstack1, itemstack);
-				} else if (!this.mergeItemStack(itemstack1, 0, 3, false)) {
-					if (index < 3 + 27) {
-						if (!this.mergeItemStack(itemstack1, 3 + 27, this.inventorySlots.size(), true)) {
-							return ItemStack.EMPTY;
-						}
-					} else {
-						if (!this.mergeItemStack(itemstack1, 3, 3 + 27, false)) {
-							return ItemStack.EMPTY;
-						}
-					}
-					return ItemStack.EMPTY;
-				}
-				if (itemstack1.getCount() == 0) {
-					slot.putStack(ItemStack.EMPTY);
-				} else {
-					slot.onSlotChanged();
-				}
-				if (itemstack1.getCount() == itemstack.getCount()) {
-					return ItemStack.EMPTY;
-				}
-				slot.onTake(playerIn, itemstack1);
-			}
-			return itemstack;
-		}
-
-		@Override /**
-					 * Merges provided ItemStack with the first avaliable one in the
-					 * container/player inventor between minIndex (included) and maxIndex
-					 * (excluded). Args : stack, minIndex, maxIndex, negativDirection. /!\ the
-					 * Container implementation do not check if the item is valid for the slot
-					 */
-		protected boolean mergeItemStack(ItemStack stack, int startIndex, int endIndex, boolean reverseDirection) {
-			boolean flag = false;
-			int i = startIndex;
-			if (reverseDirection) {
-				i = endIndex - 1;
-			}
-			if (stack.isStackable()) {
-				while (!stack.isEmpty()) {
-					if (reverseDirection) {
-						if (i < startIndex) {
-							break;
-						}
-					} else if (i >= endIndex) {
-						break;
-					}
-					Slot slot = this.inventorySlots.get(i);
-					ItemStack itemstack = slot.getStack();
-					if (slot.isItemValid(itemstack) && !itemstack.isEmpty() && areItemsAndTagsEqual(stack, itemstack)) {
-						int j = itemstack.getCount() + stack.getCount();
-						int maxSize = Math.min(slot.getSlotStackLimit(), stack.getMaxStackSize());
-						if (j <= maxSize) {
-							stack.setCount(0);
-							itemstack.setCount(j);
-							slot.putStack(itemstack);
-							flag = true;
-						} else if (itemstack.getCount() < maxSize) {
-							stack.shrink(maxSize - itemstack.getCount());
-							itemstack.setCount(maxSize);
-							slot.putStack(itemstack);
-							flag = true;
-						}
-					}
-					if (reverseDirection) {
-						--i;
-					} else {
-						++i;
-					}
-				}
-			}
-			if (!stack.isEmpty()) {
-				if (reverseDirection) {
-					i = endIndex - 1;
-				} else {
-					i = startIndex;
-				}
-				while (true) {
-					if (reverseDirection) {
-						if (i < startIndex) {
-							break;
-						}
-					} else if (i >= endIndex) {
-						break;
-					}
-					Slot slot1 = this.inventorySlots.get(i);
-					ItemStack itemstack1 = slot1.getStack();
-					if (itemstack1.isEmpty() && slot1.isItemValid(stack)) {
-						if (stack.getCount() > slot1.getSlotStackLimit()) {
-							slot1.putStack(stack.split(slot1.getSlotStackLimit()));
-						} else {
-							slot1.putStack(stack.split(stack.getCount()));
-						}
-						slot1.onSlotChanged();
-						flag = true;
-						break;
-					}
-					if (reverseDirection) {
-						--i;
-					} else {
-						++i;
-					}
-				}
-			}
-			return flag;
-		}
-
-		@Override
-		public void onContainerClosed(PlayerEntity playerIn) {
-			super.onContainerClosed(playerIn);
-			if (!bound && (playerIn instanceof ServerPlayerEntity)) {
-				if (!playerIn.isAlive() || playerIn instanceof ServerPlayerEntity && ((ServerPlayerEntity) playerIn).hasDisconnected()) {
-					for (int j = 0; j < internal.getSlots(); ++j) {
-						playerIn.dropItem(internal.extractItem(j, internal.getStackInSlot(j).getCount(), false), false);
-					}
-				} else {
-					for (int i = 0; i < internal.getSlots(); ++i) {
-						playerIn.inventory.placeItemBackInInventory(playerIn.world,
-								internal.extractItem(i, internal.getStackInSlot(i).getCount(), false));
-					}
-				}
-			}
-		}
-
-		private void slotChanged(int slotid, int ctype, int meta) {
-			if (this.world != null && this.world.isRemote) {
-				AllTheElementsMod.PACKET_HANDLER.sendToServer(new GUISlotChangedMessage(slotid, x, y, z, ctype, meta));
-				handleSlotAction(entity, slotid, ctype, meta, x, y, z);
-			}
 		}
 	}
 
@@ -308,7 +112,7 @@ public class ForgeGuiGui extends AllTheElementsModElements.ModElement {
 			this.xSize = 176;
 			this.ySize = 166;
 		}
-		private static final ResourceLocation texture = new ResourceLocation("all_the_elements:textures/forge_gui.png");
+		private static final ResourceLocation texture = new ResourceLocation("all_the_elements:textures/explaination.png");
 		@Override
 		public void render(int mouseX, int mouseY, float partialTicks) {
 			this.renderBackground();
@@ -323,8 +127,10 @@ public class ForgeGuiGui extends AllTheElementsModElements.ModElement {
 			int k = (this.width - this.xSize) / 2;
 			int l = (this.height - this.ySize) / 2;
 			this.blit(k, l, 0, 0, this.xSize, this.ySize, this.xSize, this.ySize);
-			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("all_the_elements:textures/flame_on.png"));
-			this.blit(this.guiLeft + 17, this.guiTop + 31, 0, 0, 16, 16, 16, 16);
+			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("all_the_elements:textures/coal.png"));
+			this.blit(this.guiLeft + 3, this.guiTop + 26, 0, 0, 16, 16, 16, 16);
+			Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("all_the_elements:textures/better_koal.png"));
+			this.blit(this.guiLeft + 3, this.guiTop + 43, 0, 0, 16, 16, 16, 16);
 		}
 
 		@Override
@@ -343,7 +149,11 @@ public class ForgeGuiGui extends AllTheElementsModElements.ModElement {
 
 		@Override
 		protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
-			this.font.drawString("Inventory", 6, 70, -12829636);
+			this.font.drawString("How does the forge works ? ", 2, 3, -12829636);
+			this.font.drawString("=", 20, 30, -12829636);
+			this.font.drawString("700\u00BAC", 27, 30, -12829636);
+			this.font.drawString("=", 20, 46, -12829636);
+			this.font.drawString("1200\u00BAC", 27, 46, -12829636);
 		}
 
 		@Override
@@ -356,13 +166,9 @@ public class ForgeGuiGui extends AllTheElementsModElements.ModElement {
 		public void init(Minecraft minecraft, int width, int height) {
 			super.init(minecraft, width, height);
 			minecraft.keyboardListener.enableRepeatEvents(true);
-			this.addButton(new Button(this.guiLeft + 65, this.guiTop + 28, 50, 20, "Smelt", e -> {
+			this.addButton(new Button(this.guiLeft + 152, this.guiTop + 4, 19, 20, "X", e -> {
 				AllTheElementsMod.PACKET_HANDLER.sendToServer(new ButtonPressedMessage(0, x, y, z));
 				handleButtonAction(entity, 0, x, y, z);
-			}));
-			this.addButton(new Button(this.guiLeft + 116, this.guiTop + 28, 30, 20, "?", e -> {
-				AllTheElementsMod.PACKET_HANDLER.sendToServer(new ButtonPressedMessage(1, x, y, z));
-				handleButtonAction(entity, 1, x, y, z);
 			}));
 		}
 	}
@@ -456,22 +262,12 @@ public class ForgeGuiGui extends AllTheElementsModElements.ModElement {
 		if (buttonID == 0) {
 			{
 				Map<String, Object> $_dependencies = new HashMap<>();
-				$_dependencies.put("x", x);
-				$_dependencies.put("y", y);
-				$_dependencies.put("z", z);
-				$_dependencies.put("world", world);
-				TestProcedure.executeProcedure($_dependencies);
-			}
-		}
-		if (buttonID == 1) {
-			{
-				Map<String, Object> $_dependencies = new HashMap<>();
 				$_dependencies.put("entity", entity);
 				$_dependencies.put("x", x);
 				$_dependencies.put("y", y);
 				$_dependencies.put("z", z);
 				$_dependencies.put("world", world);
-				OpenForgeHelpGuiProcedure.executeProcedure($_dependencies);
+				OpenForgeGuiProcedure.executeProcedure($_dependencies);
 			}
 		}
 	}
