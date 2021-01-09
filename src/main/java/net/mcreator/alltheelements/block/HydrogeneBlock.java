abc0a4a968cfeb540ea92192c1c3e6b52c877594
ZetPin <Usuari@91.126.232.244>
2021-01-09 16:03:40 +0100

    Patch 4.9 : Changed Hydrogene Generation

--- src/main/java/net/mcreator/alltheelements/block/HydrogeneBlock.java
+++ src/main/java/net/mcreator/alltheelements/block/HydrogeneBlock.java
@@ -2,9 +2,7 @@
 package net.mcreator.alltheelements.block;
 
 import net.minecraftforge.registries.ObjectHolder;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fluids.ForgeFlowingFluid;
 import net.minecraftforge.fluids.FluidAttributes;
@@ -13,16 +11,6 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
-import net.minecraft.world.gen.placement.Placement;
-import net.minecraft.world.gen.placement.ChanceConfig;
-import net.minecraft.world.gen.feature.LakesFeature;
-import net.minecraft.world.gen.feature.BlockStateFeatureConfig;
-import net.minecraft.world.gen.GenerationStage;
-import net.minecraft.world.gen.ChunkGenerator;
-import net.minecraft.world.dimension.DimensionType;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.IWorld;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.item.Items;
 import net.minecraft.item.ItemGroup;
@@ -37,8 +25,6 @@ import net.minecraft.block.FlowingFluidBlock;
 import net.minecraft.block.Block;
 
 import net.mcreator.alltheelements.AllTheElementsModElements;
-
-import java.util.Random;
 
 @AllTheElementsModElements.ModElement.Tag
 public class HydrogeneBlock extends AllTheElementsModElements.ModElement {
@@ -79,24 +65,5 @@ public class HydrogeneBlock extends AllTheElementsModElements.ModElement {
 		}.setRegistryName("hydrogene"));
 		elements.items.add(() -> new BucketItem(still, new Item.Properties().containerItem(Items.BUCKET).maxStackSize(1).group(ItemGroup.MISC))
 				.setRegistryName("hydrogene_bucket"));
-	}
-
-	@Override
-	public void init(FMLCommonSetupEvent event) {
-		for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
-			biome.addFeature(GenerationStage.Decoration.LOCAL_MODIFICATIONS, new LakesFeature(BlockStateFeatureConfig::deserialize) {
-				@Override
-				public boolean place(IWorld world, ChunkGenerator generator, Random rand, BlockPos pos, BlockStateFeatureConfig config) {
-					DimensionType dimensionType = world.getDimension().getType();
-					boolean dimensionCriteria = false;
-					if (dimensionType == DimensionType.OVERWORLD)
-						dimensionCriteria = true;
-					if (!dimensionCriteria)
-						return false;
-					return super.place(world, generator, rand, pos, config);
-				}
-			}.withConfiguration(new BlockStateFeatureConfig(block.getDefaultState()))
-					.withPlacement(Placement.WATER_LAKE.configure(new ChanceConfig(1))));
-		}
 	}
 }
